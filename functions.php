90b377824222d5ed85ec61e0bee644eee47bb34a
Robert Clarke <robert@brightredmedia.com>
2023-11-02 15:46:47 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -32,3 +32,13 @@ require_once $filepath;
 }
 
 unset($file, $filepath);
+
+function general_admin_notice(){
+    global $pagenow;
+    if ( $pagenow == 'options-general.php' ) {
+         echo '<div class="notice notice-warning is-dismissible">
+             <p>This is an example of a notice that appears on the settings page.</p>
+         </div>';
+    }
+}
+add_action('admin_notices', 'general_admin_notice');
